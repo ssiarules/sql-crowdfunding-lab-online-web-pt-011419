60fdd04fce0c8a4e99d23998eb0393094100bdd8
Johnson Joseph <jhonsonj88@gmail.com>
2019-03-28 05:13:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,5 +29,4 @@ end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 "SELECT projects.category, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'books';"
-#"SELECT projects.category, sum(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'books'"
 end
